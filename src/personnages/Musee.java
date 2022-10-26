a871320565b60561cf92217118d836cf3d4f3904
Daoud LAMALMI <102736413+MadokaIII@users.noreply.github.com>
2022-10-26 11:57:28 +0200

    musee fix

--- src/personnages/Musee.java
+++ src/personnages/Musee.java
@@ -2,15 +2,14 @@ package personnages;
 
 public class Musee {
 	
-	static Trophee[] collection = new Trophee[200];
+	Trophee[] collection = new Trophee[200];
 	int nbTrophee = 0;
 	
-	public void donnerTrophees(Gaulois gaulois, Equipement equipement) {
+	public void donnerTrophees(Gaulois gaulois, Trophee trophee) {
         if (nbTrophee >= 200) {
-            throw new IllegalArgumentException("Musee complet, trophee rejeté :" + equipement.getNom());
+            throw new IllegalArgumentException("Musee complet, trophee rejeté :" + trophee.getEquipement());
         } else {
-            Trophee trophee = new Trophee(gaulois, equipement);
-            collection[nbTrophee] = trophee;
+        	collection[nbTrophee] = trophee;
             nbTrophee++;
 //            System.out.println(gaulois.getNom() + "vient d'ajouter le trophee " + equipement.getNom() + " au musee.\n");
         }
